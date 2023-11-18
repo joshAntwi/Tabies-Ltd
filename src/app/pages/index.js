01eb9a580428a105aa184dc922f7a23a737bb5b7
joshAntwi <josh.antwiboasiako@yahoo.com>
2023-11-18 20:06:31 +0000

    initial commit

---
+++ src/app/pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>Hello Next.js</h1>
+      <p>Let's explore different ways to style Next.js apps</p>
+    </div>
+  )
+}
